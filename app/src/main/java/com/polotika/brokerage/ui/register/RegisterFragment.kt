d7a15c44e4b36f1ba20cf42f6b8a55aefebfe4c1
ahmed <ahmed.mostfa.ghorab@gmail.com>
2021-09-17 17:18:42 +0200

    RegisterViewModel.kt

--- app/src/main/java/com/polotika/brokerage/ui/register/RegisterFragment.kt
+++ app/src/main/java/com/polotika/brokerage/ui/register/RegisterFragment.kt
@@ -3,20 +3,22 @@ package com.polotika.brokerage.ui.register
 import android.app.ActivityOptions
 import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
+import android.os.Message
+import android.util.Log
 import android.view.View
-import android.view.ViewGroup
 import androidx.lifecycle.Observer
-import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputLayout
 import com.polotika.brokerage.MainActivity
 import com.polotika.brokerage.R
 import com.polotika.brokerage.base.BaseFragment
 import com.polotika.brokerage.databinding.FragmentRegisterBinding
 
 class RegisterFragment : BaseFragment<FragmentRegisterBinding,RegisterViewModel>() {
+    private val TAG = "RegisterFragment"
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.vm = viewModel
         observers()
     }
 
@@ -27,9 +29,37 @@ class RegisterFragment : BaseFragment<FragmentRegisterBinding,RegisterViewModel>
                     finishRegister()
                 }
                 RegisterNavigationState.LoginFailed->{
-                    showMessage("Please fulfill all fields!")
+                    Snackbar.make(binding.root,getString(R.string.register_error),Snackbar.LENGTH_LONG).show()
                 }
             }
+        })
+        viewModel.firstNameError.observe(viewLifecycleOwner, Observer {
+            Log.d(TAG, "observers: firstName $it")
+            showViewError(binding.firstNameTil,getString(R.string.first_name_error),it)
+        })
+        viewModel.lastNameError.observe(viewLifecycleOwner, Observer {
+            Log.d(TAG, "observers: lastName $it")
+            showViewError(binding.lastNameTil, getString(R.string.last_name_error),it)
+        })
+        viewModel.emailError.observe(viewLifecycleOwner, Observer {
+            showViewError( binding.emailTil, getString(R.string.email_error),it)
+
+        })
+        viewModel.mobileError.observe(viewLifecycleOwner, Observer {
+            showViewError(binding.mobileTil,getString(R.string.mobile_error),it)
+        })
+        viewModel.passwordError.observe(viewLifecycleOwner, Observer {
+
+            showViewError(binding.passwordTil,getString(R.string.password_error),it)
+
+        })
+        viewModel.isChecked.observe(viewLifecycleOwner, Observer {
+            when(it){
+                false -> binding.termsCb.error = getString(R.string.terms_error)
+                true -> binding.termsCb.error = null
+            }
+
+
         })
     }
     private fun finishRegister(){
@@ -41,8 +71,12 @@ class RegisterFragment : BaseFragment<FragmentRegisterBinding,RegisterViewModel>
         )
         requireActivity().finish()
     }
-
-
+    private fun showViewError(view:TextInputLayout,message: String?, isError:Boolean){
+        view.isErrorEnabled = isError
+        if (isError) {
+            view.isErrorEnabled = isError
+            view.error = message}
+    }
 
     override fun initializeLayout() = R.layout.fragment_register
     override fun initializeViewModel() = RegisterViewModel::class.java
--- app/src/main/java/com/polotika/brokerage/ui/register/RegisterViewModel.kt
+++ app/src/main/java/com/polotika/brokerage/ui/register/RegisterViewModel.kt
@@ -1,5 +1,7 @@
 package com.polotika.brokerage.ui.register
 
+import android.util.Log
+import android.util.Patterns
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.polotika.brokerage.base.BaseNavigator
@@ -11,6 +13,7 @@ import com.polotika.brokerage.ui.login.LoginNavigationState
 import com.polotika.brokerage.utils.PreferencesUtils
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
+import java.util.regex.Pattern
 import javax.inject.Inject
 
 @HiltViewModel
@@ -19,25 +22,38 @@ class RegisterViewModel @Inject constructor(
     private val registerUseCase: RegisterUseCase
 ) : BaseViewModel<BaseNavigator>() {
 
+    private val TAG = "RegisterViewModel"
+    //^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{4,}$
+    private val PASSWORD_PETTERN = "^(?=.*[0-9])(?=.*[@#$%^&+=])(?=\\S+$).{4,}$"
+
     val firstName = MutableLiveData<String>()
     val mobileNumber = MutableLiveData<String>()
     val email = MutableLiveData<String>()
     val password = MutableLiveData<String>()
     val lastName = MutableLiveData<String>()
+    val isChecked = MutableLiveData<Boolean>(true)
+
+    val firstNameError = MutableLiveData<Boolean>()
+    val lastNameError = MutableLiveData<Boolean>()
+    val emailError = MutableLiveData<Boolean>()
+    val passwordError = MutableLiveData<Boolean>()
+    val mobileError = MutableLiveData<Boolean>()
 
     val registerNavigationEvent = MutableLiveData<RegisterNavigationState>()
 
     fun register() {
+        Log.d(TAG, "register: ${isDataValid()}")
         if (isDataValid()) {
             when (registerUseCase.execute(collectUserData())) {
                 Event.Success -> {
                     completeRegister()
                 }
                 Event.Failed -> {
-                    registerNavigationEvent.value= RegisterNavigationState.LoginFailed
+                    registerNavigationEvent.value = RegisterNavigationState.LoginFailed
                 }
             }
         } else {
+            showViewErrors()
             registerNavigationEvent.value = RegisterNavigationState.LoginFailed
         }
     }
@@ -50,9 +66,21 @@ class RegisterViewModel @Inject constructor(
     }
 
     private fun isDataValid(): Boolean {
-        return (!firstName.value.isNullOrBlank() && lastName.value.isNullOrBlank() &&
-                email.value.isNullOrBlank() && mobileNumber.value.isNullOrBlank() &&
-                password.value.isNullOrBlank())
+        return !(firstName.value.isNullOrBlank() || lastName.value.isNullOrBlank() ||
+                email.value.isNullOrBlank() || mobileNumber.value.isNullOrBlank() ||
+                password.value.isNullOrBlank() || isChecked.value == false ||
+                !Pattern.compile(PASSWORD_PETTERN).matcher(password.value.toString()).matches()||
+                !Patterns.EMAIL_ADDRESS.matcher(email.value.toString()).matches())
+    }
+
+    private fun showViewErrors() {
+        firstNameError.value = firstName.value.isNullOrEmpty()
+        lastNameError.value = lastName.value.isNullOrEmpty()
+        emailError.value = email.value.isNullOrEmpty() || !Patterns.EMAIL_ADDRESS.matcher(email.value.toString()).matches()
+        mobileError.value = mobileNumber.value?.length?:0 < 11
+        passwordError.value = password.value?.length?:0 < 8||
+                !Pattern.compile(PASSWORD_PETTERN).matcher(password.value.toString()).matches()
+
     }
 
     private fun collectUserData(): User {
