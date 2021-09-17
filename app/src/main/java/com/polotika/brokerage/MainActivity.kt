87d9aa077eed1937d901e38e2d897cb3f208b6da
ahmed <ahmed.mostfa.ghorab@gmail.com>
2021-09-17 23:21:51 +0200

    Home Navigation

--- app/src/main/java/com/polotika/brokerage/MainActivity.kt
+++ app/src/main/java/com/polotika/brokerage/MainActivity.kt
@@ -1,26 +1,68 @@
 package com.polotika.brokerage
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.polotika.brokerage.databinding.ActivityMainBinding
+import com.polotika.brokerage.utils.PreferencesUtils
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-    lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navController: NavController
+    private lateinit var toggle: ActionBarDrawerToggle
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val navHost = supportFragmentManager.findFragmentById(R.id.homeNavHostFragment) as NavHostFragment
-        navController = navHost.navController
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        setupActionBarWithNavController(navController)
+        toggle = ActionBarDrawerToggle(
+            this,
+            binding.drawerLayout,
+            R.string.drawer_open,
+            R.string.drawer_close
+        )
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val navHost =
+            supportFragmentManager.findFragmentById(R.id.homeNavHostFragment) as NavHostFragment
+        navController = navHost.navController
+        binding.navView.setupWithNavController(navController)
+        appBarConfiguration = AppBarConfiguration(navController.graph, binding.drawerLayout)
+
+
+        binding.mainContent.bottomNav.setupWithNavController(navController)
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp() || super.onSupportNavigateUp()
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+
+    fun logout(item: MenuItem) {
+        lifecycleScope.launch {
+            PreferencesUtils(this@MainActivity).userLogout()
+        }
+        startActivity(Intent(this, LoginActivity::class.java))
     }
 
 }
